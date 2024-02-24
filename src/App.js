eaba01b6aaabea43c2f30ca4514892bc35574750
Indira <indira.marpaka@gmail.com>
2024-02-24 19:07:46 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import './App.css';
+
+const App = () => {
+
+  
+
+  return (
+    <div className="App">
+      <Header />
+      <div className="Content">
+        <p>
+          
+          <h1>Accounts Managment</h1>Hello</p>
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
